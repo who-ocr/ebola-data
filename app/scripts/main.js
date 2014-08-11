972ab0f4c7b285975d713364938d23a8f3c8712a
Derek Lieu <derek@developmentseed.org>
2014-08-11 15:05:01 -0400

    Initial scaffolding

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Who, $*/
+
+
+window.Who = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Who.init();
+});
