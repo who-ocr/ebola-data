d9b7be6b7a563307f8c4d0ccbd259d5a6ba03227
Derek Lieu <derek@developmentseed.org>
2014-08-13 18:10:39 -0400

    Toggle view

---
+++ test/views/dropdown.spec.js
@@ -0,0 +1,10 @@
+/*global beforeEach, describe, it, assert, expect  */
+'use strict';
+
+describe('Dropdown View', function () {
+
+    beforeEach(function () {
+        this.DropdownView = new WHO.Views.Dropdown();
+    });
+
+});
