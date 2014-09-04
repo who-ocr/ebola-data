30637ef40419deff7e98dad1da73a5e8b0c63ca3
Nate Smith <nate@developmentseed.org>
2014-09-04 01:18:48 -0400

    build and build fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -204,6 +204,7 @@ module.exports = function (grunt) {
                         '.htaccess',
                         'images/{,*/}*.{webp,gif}',
                         'styles/fonts/{,*/}*.*',
+                        'fonts/{,*/}*.*'
                     ]
                 }]
             }
